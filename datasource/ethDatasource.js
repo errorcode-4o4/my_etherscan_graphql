f22777fb850b8b48971066c6e7f324fe0ef1e39d
Avinash Kumar <avinashk.ug20.ee@nitp.ac.in>
2023-11-21 14:18:00 +0530

    Add files via upload

---
+++ datasource/ethDatasource.js
@@ -0,0 +1,40 @@
+const { RESTDataSource } = require("apollo-datasource-rest");
+
+const eth_address = "0xd8dA6BF26964aF9D7eEd9e03E53415D37aA96045";
+
+class EtherDataSource extends RESTDataSource {
+  constructor() {
+    super();
+    this.baseURL = "https://api.etherscan.io/api";
+  }
+
+  async etherBalanceByAddress() {
+    // Get ether balance by address
+    return this.get(
+      `?module=account&action=balance&address=${eth_address}&tag=latest&apikey=${process.env.ETHERSCAN_API}`
+    );
+  }
+
+  async totalSupplyOfEther() {
+    // Get total ether supply
+    return this.get(
+      `?module=stats&action=ethsupply&apikey=${process.env.ETHERSCAN_API}`
+    );
+  }
+
+  async getLatestEthereumPrice() {
+    // Get latest Ethereum price
+    return this.get(
+      `?module=stats&action=ethprice&apikey=${process.env.ETHERSCAN_API}`
+    );
+  }
+
+  async getBlockConfirmationTime() {
+    // Get block confirmation time
+    return this.get(
+      `?module=gastracker&action=gasestimate&gasprice=2000000000&apikey=${process.env.ETHERSCAN_API}`
+    );
+  }
+}
+
+module.exports = EtherDataSource;
